c352b507fe67dd5b1c41ad902eb4d3ed38994466
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2023-12-29 21:54:02 +0530

    added strings

---
+++ chai aur js/basics/05_strings.js
@@ -0,0 +1,19 @@
+let string = "hello" + "world";
+console.log(string);
+
+const repocount = 20;
+
+console.log(`hello my name is ${string} and my repo count is ${repocount}`);
+
+let gamename = "saunak";
+const newstring = gamename.substring(0, 3);
+console.log(newstring);
+
+let newstring2 = gamename.slice(0, 3);
+console.log(newstring2);
+
+const newstring3 = gamename.replace("sau", "abc");
+console.log(newstring3);
+
+const newstring4 = gamename.toUpperCase();
+console.log(newstring4);
--- chai aur js/basics/summary_datatypes.js
+++ chai aur js/basics/summary_datatypes.js
@@ -2,39 +2,49 @@
 
 // String, Number, Boolean , null, undefined, Symbol , BigInt ,
 
-const score = 100;
-console.log(score);
-const scorevalue = 100.3;
-console.log(scorevalue);
-const isloggedin = false;
-console.log(isloggedin);
-const outsidetemp = null;
-console.log(outsidetemp);
-let useremail;
+// const score = 100;
+// console.log(score);
+// const scorevalue = 100.3;
+// console.log(scorevalue);
+// const isloggedin = false;
+// console.log(isloggedin);
+// const outsidetemp = null;
+// console.log(outsidetemp);
+// let useremail;
 
-const id = Symbol("213");
-console.log(id);
-const anotherid = Symbol("213");
-console.log(anotherid);
-console.log(id === anotherid);
+// const id = Symbol("213");
+// console.log(id);
+// const anotherid = Symbol("213");
+// console.log(anotherid);
+// console.log(id === anotherid);
 
-const bigno = 2131243242223n;
+// const bigno = 2131243242223n;
 
-// reference (non primitive)
+// // reference (non primitive)
 
-// Array , Object , Functions ,
+// // Array , Object , Functions ,
 
-const heroes = ["ultraman", "powerranger", "bsdk"];
+// const heroes = ["ultraman", "powerranger", "bsdk"];
 
-let obj = {
-  name: "saunak",
-  age: 21,
-};
+// let obj = {
+//   name: "saunak",
+//   age: 21,
+// };
 
-const myfunc = function () {
-  console.log("hello world");
-};
+// const myfunc = function () {
+//   console.log("hello world");
+// };
 
-console.log(bigno);
-console.log(heroes);
-console.log(myfunc);
+// console.log(bigno);
+// console.log(heroes);
+// console.log(myfunc);
+
+//stack (primitive) , heap (non-primitive)
+
+let youtubename = "saunak";
+
+let anothhername = youtubename;
+anothhername = "abc";
+
+console.log(youtubename);
+console.log(anothhername);
