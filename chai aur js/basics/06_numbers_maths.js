ab1e0a9763eadc9766b1d56965e40c7912c5b342
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2023-12-30 10:55:06 +0530

    added numbers and maths , dates

---
+++ chai aur js/basics/06_numbers_maths.js
@@ -0,0 +1,25 @@
+let score = 500;
+const balance = new Number(100);
+console.log(score);
+console.log(balance);
+
+const no = 25.213213;
+console.log(no.toPrecision(4));
+
+// maths-------------
+
+console.log(Math.PI);
+console.log(Math.round(4.5));
+
+// most used-----------
+
+console.log(Math.random() * 100);
+console.log(Math.random() * 100 + 1);
+console.log(Math.floor(Math.random() * 100 + 1));
+console.log(Math.round(Math.random() * 100));
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
+
