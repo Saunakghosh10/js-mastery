89a9381b1063e58c1d88a9c91ff21aa12af79f48
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2024-01-06 23:21:35 +0530

    iterations

---
+++ chai aur js/05_iterations/highorderloops.js
@@ -0,0 +1,20 @@
+//for of
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (const iterator of arr) {
+  //   console.log(iterator);
+}
+
+const string = "hello";
+for (const iterator of string) {
+  //   console.log(iterator);
+}
+
+// maps
+const map = new Map();
+map.set("name", "john");
+map.set("age", 25);
+map.set("email", "<EMAIL>");
+map.set("address", "<ADDRESS>");
+map.set("phone", "<PHONE>");
+console.log(map);
---
+++ chai aur js/05_iterations/loops.js
@@ -0,0 +1,36 @@
+for (let i = 0; i < 10; i++) {
+  const element = i;
+
+  //   console.log(element);
+}
+
+for (let i = 0; i < 10; i++) {
+  //   console.log(`outer loop is: ${i}`);
+  for (let j = 0; j < 10; j++) {
+    // console.log(`inner loop is: ${j} and outer loop is: ${i}`);
+    // console.log(i + "*" + j + "=" + i * j);
+  }
+}
+
+// while
+let i = 0;
+while (i < 10) {
+  //   console.log(i);
+  i++;
+}
+
+let superhero = ["batman", "superman", "wonderwoman", "flash"];
+
+let arr = 0;
+while (arr < superhero.length) {
+//   console.log(superhero[arr]);
+  arr++;
+}
+
+// do while
+let j = 0;
+do {
+//   console.log(j);
+  j++;
+} while (j < 10);
+
