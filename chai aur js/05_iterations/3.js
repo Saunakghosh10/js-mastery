388fbcdc0abac4fa43c66cfb0eb677535caa7f2c
Saunak <76943154+Saunakghosh10@users.noreply.github.com>
2024-01-07 23:02:45 +0530

    map and reduce

---
+++ chai aur js/05_iterations/3.js
@@ -0,0 +1,15 @@
+const coding = ["js", "ruby", "python", "java", "c++", "c#", "php"];
+
+const values = coding.forEach((value) => {
+  console.log(value);
+});
+
+// const mynums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const newnums = mynums.filter((mynums) => {
+//   return mynums > 5;
+// });
+// const newnums2 = mynums.map((mynums) => {
+//   return mynums * 2;
+// });
+
+// console.log(newnums);
